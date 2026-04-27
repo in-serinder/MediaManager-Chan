29778cb5d1075a651a11cd4f96521fdc2695f1d5
Serinder <serinder@163.com>
2026-04-27 19:08:06 +0800

    增加音频格式支持

--- MediaManager-Chan/Form1.cs
+++ MediaManager-Chan/Form1.cs
@@ -157,7 +157,7 @@ namespace MediaManager_Chan
 
         private void loadVideos()
         {
-            allfiles = utility.SortFiles(utility.GetFilesByExtension(currentopen_path, ["mp4","mov","avi","mp3","flv","mkv"]), sort_cb.Text.ToString());
+            allfiles = utility.SortFiles(utility.GetFilesByExtension(currentopen_path, ["mp4","mov","avi","mp3","flv","mkv","wav","flac","midi"]), sort_cb.Text.ToString());
             view_lb.Text = "ÊýÁ¿:" + allfiles.Length;
             videolist_cb.Items.Clear();
             foreach (string file in allfiles)
